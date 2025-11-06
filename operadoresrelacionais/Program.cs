7045daf0df0c194046560686d787dc80abfa16e8
MATHEUS GOMES DE OLIVEIRA <matheus.goliveira28@senacsp.edu.br>
2025-11-06 14:58:09 -0300

    enviando atualização

---
+++ operadoresrelacionais/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 10;
+int b = 5;
+
+//Operadores Relacionais
+bool maiorQue = a > b;
+bool menirQue = a < b;
+bool igual = a == b;
+bool diferente = a != b;
+bool maiorOuIgual = a >= b;
+bool menorOuIgual = a <= b;
+
+// Exibindo resultados dos operadores relacionais
+Console.WriteLine("\nOperadores Relacionais:");
+Console.WriteLine($"{a} > {b}: {maiorQue}");
+Console.WriteLine($"{a} < {b}: {menirQue}");
+Console.WriteLine($"{a} == {b}: {igual}");
+Console.WriteLine($"{a} != {b}: {diferente}");
+Console.WriteLine($"{a} >= {b}: {maiorOuIgual}");
+Console.WriteLine($"{a} <= {b}: {menorOuIgual}");
